0100e20d1933cb7e5ae5e4054d34434ec587dd64
Jean Karel <mailjeank@gmail.com>
2023-04-25 09:54:11 -0500

    Initial project commit

---
+++ hiitfire/hiitfireApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiitfireApp.swift
+//  hiitfire
+//
+//  Created by Jean Karel on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct hiitfireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
