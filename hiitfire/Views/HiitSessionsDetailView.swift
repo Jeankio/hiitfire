d3fe3092ca5522caf38e523dd33ed750043963a8
Jean Karel <mailjeank@gmail.com>
2023-04-29 17:35:22 -0500

    Improvements to HiitSessionsDetailView

--- hiitfire/Views/HiitSessionsDetailView.swift
+++ hiitfire/Views/HiitSessionsDetailView.swift
@@ -1,9 +1,3 @@
-//
-//  HiitSessionsDetailView.swift
-//  hiitfire
-//
-//  Created by Jean Karel on 26/04/23.
-//
 
 import SwiftUI
 
@@ -16,79 +10,87 @@ struct HiitSessionsDetailView: View {
     @State private var isPresentingEditView = false
     
     
+    
     var body: some View {
-        List {
-            Section(header: Text("Training info")) {
-                // Navegacion desde Detailview a HiitTrainingView
-                NavigationLink(destination: HiitTrainingView(exercise: $exercise)) {
-                    Label("Start HIIT session", systemImage: "timer.square")
-                        .font(.headline)
-                        .foregroundColor(.accentColor)
+        VStack {
+            List {
+                Section(header: Text("Training info")) {
+                    HStack {
+                        Label("Duration", systemImage: "clock")
+                        Spacer()
+                        Text("\(exercise.duration) minutes")
+                    }
+                    HStack {
+                        Label("Set color", systemImage: "paintpalette")
+                        Spacer()
+                        Text(exercise.theme.name)
+                            .padding(4)
+                            .foregroundColor(exercise.theme.accentColor)
+                            .background(exercise.theme.mainColor)
+                            .cornerRadius(4)
+                    }
                 }
-                
-                HStack {
-                    Label("Duration", systemImage: "clock")
-                    Spacer()
-                    Text("\(exercise.duration) minutes")
+                Section(header: Text("You will burn")) {
+                    //Se llama a la funcion de las calorias
+                    Label("\(exercise.calories) KCal", systemImage: "flame.fill")
+                        .bold()
+                        .font(.system(size: 30))
+                        .frame(maxWidth:.infinity, alignment: .center)
                 }
-                HStack {
-                    Label("Set color", systemImage: "paintpalette")
-                    Spacer()
-                    Text(exercise.theme.name)
-                        .padding(4)
-                        .foregroundColor(exercise.theme.accentColor)
-                        .background(exercise.theme.mainColor)
-                        .cornerRadius(4)
-                }
-            }
-            
-            Section(header: Text("You will burn")) {
-                //Se llama a la funcion de las calorias
-                Label("\(exercise.calories) Calories", systemImage: "flame.fill")
-                    .bold()
-                    .font(.system(size: 18))
-                //.frame(maxWidth:.infinity, alignment: .center)
-            }
-            
-            
-            Section(header: Text("HIIT exercises")) {
-                ForEach(exercise.exercises) { exercise in
-                    Label(exercise.name, systemImage: "figure.dance")
+            } // List
+            .navigationTitle(exercise.title)
+            .toolbar {
+                Button("Edit") {
+                    isPresentingEditView = true
+                    editingSession = exercise
                 }
             }
-        }
-        .navigationTitle(exercise.title)
-        .toolbar {
-            Button("Edit") {
-                isPresentingEditView = true
-                editingSession = exercise
-            }
-        }
-        // sheet for editview
-        .sheet(isPresented: $isPresentingEditView) {
-            NavigationStack {
-                //Actualizar HSEV con el binding:
-                HiitSessionsEditView(exercise: $editingSession)
-                    .navigationBarTitle(exercise.title)
-                //Cancelar la edicion
-                    .toolbar {
-                        ToolbarItem(placement: .cancellationAction) {
-                            Button("Cancel") {
-                                isPresentingEditView = false
+            // sheet for editview
+            .sheet(isPresented: $isPresentingEditView) {
+                NavigationStack {
+                    //Actualizar HSEV con el binding:
+                    HiitSessionsEditView(exercise: $editingSession)
+                        .navigationBarTitle(exercise.title)
+                    //Cancelar la edicion
+                        .toolbar {
+                            ToolbarItem(placement: .cancellationAction) {
+                                Button("Cancel") {
+                                    isPresentingEditView = false
+                                }
+                            }
+                            // Guardar la edicion
+                            ToolbarItem(placement: .confirmationAction) {
+                                Button("Done") {
+                                    isPresentingEditView = false
+                                    exercise = editingSession
+                                }
                             }
                         }
-                        // Guardar la edicion
-                        ToolbarItem(placement: .confirmationAction) {
-                            Button("Done") {
-                                isPresentingEditView = false
-                                exercise = editingSession
-                        }
+                }
+            }
+        } // Vstack
+        VStack {
+            List {
+                Section(header: Text("Your exercises")) {
+                    ForEach(exercise.exercises) { exercise in
+                        Label(exercise.name, systemImage: "figure.dance")
                     }
                 }
             }
         }
-    }
-}
+        VStack {
+            NavigationLink(destination: HiitTrainingView(exercise: $exercise)) {
+                Text("Go")
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(exercise.theme.accentColor)
+                    .background(exercise.theme.mainColor)
+                    .clipShape(Circle())
+                    .padding(.top, 20)
+            }
+            .shadow(radius: 10)
+        }
+    } // Var body
+}// Struct
 
 struct HiitSessionsDetailView_Previews: PreviewProvider {
     static var previews: some View {
